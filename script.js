81c71481b0d1ef839a695ebda71d4116470edfd2
Michel Morais <valentimmichel@gmail.com>
2023-05-13 17:42:46 -0300

    Implemetação da função Quantidade de dias

--- script.js
+++ script.js
@@ -1,35 +1,51 @@
+const calcularBtn = document.getElementById('calcular');
+calcularBtn.addEventListener('click', calcular);
 
-const form = document.querySelector('form');
-const inputIntervalo = document.querySelector('#intervalo');
-const btnCalcular = document.querySelector('#calcular');
-const resultado = document.querySelector('#resultado');
+function calcular(event) {
+  event.preventDefault();
 
-function calcularHorarios() {
-  const agora = new Date();
-  const intervalo = inputIntervalo.value;
+  const intervalo = document.getElementById('intervalo').value;
+  const tempo = document.getElementById('tempo').value;
 
-  // Transforma o intervalo em milissegundos
-  const intervaloMs = intervalo * 60 * 60 * 1000;
-
-  // Calcula o número de horários possíveis
-  const numHorarios = Math.floor(24 * 60 * 60 * 1000 / intervaloMs);
-
-  // Calcula todos os horários possíveis e formata como string
-  const horarios = Array(numHorarios).fill().map((_, i) => {
-    const horaTomar = new Date(agora.getTime() + (intervaloMs * i));
-    return horaTomar.toLocaleTimeString('pt-BR', { hour: 'numeric', minute: 'numeric' });
-  });
-
-  // Exibe o resultado para o usuário
-  resultado.innerHTML = `Os horários para tomar o medicamento são:<br>${horarios.join('<br>')}`;
+  if (intervalo && tempo) {
+    calcularHorarios(intervalo, tempo);
+    calcularDataFinal(intervalo, tempo);
+  }
 }
 
-btnCalcular.addEventListener('click', function(event) {
-  event.preventDefault();
-  calcularHorarios();
-});
+function calcularHorarios(intervalo, tempo) {
+  const resultadoEl = document.getElementById('resultado');
+  resultadoEl.innerHTML = '';
 
-form.addEventListener('submit', function(event) {
-  event.preventDefault();
-  calcularHorarios();
-});
+  const horaInicio = new Date();
+  horaInicio.setMinutes(horaInicio.getMinutes() + 10);
+  horaInicio.setSeconds(0);
+
+  let horaAtual = new Date(horaInicio);
+
+  const horaFim = new Date(horaInicio);
+  horaFim.setDate(horaFim.getDate() + parseInt(tempo));
+
+  const totalHorarios = 24 / parseInt(intervalo);
+
+  let horarios = '';
+  for (let i = 0; i < totalHorarios; i++) {
+    horarios += horaAtual.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' }) + '<br>';
+    horaAtual.setHours(horaAtual.getHours() + parseInt(intervalo));
+  }
+
+  resultadoEl.innerHTML = `<h2>Horários:</h2><p>${horarios}</p>`;
+}
+
+function calcularDataFinal(intervalo, tempo) {
+  const resultadoEl = document.getElementById('resultado');
+  const dataAtual = new Date();
+  const dataFinal = new Date(dataAtual.getTime() + (parseInt(tempo) -1) * 24 * 60 * 60 * 1000);
+  const diasRestantes = (24 - dataAtual.getHours()) / parseInt(intervalo);
+  dataFinal.setDate(dataFinal.getDate() + Math.ceil(diasRestantes));
+
+  const dataFormatada = `${dataFinal.getDate().toString().padStart(2, '0')}/${(dataFinal.getMonth()+1).toString().padStart(2, '0')}/${dataFinal.getFullYear()}`;
+  const dataFinalEl = document.createElement('p');
+  dataFinalEl.innerText = `Data final da medicação: ${dataFormatada}`;
+  resultadoEl.appendChild(dataFinalEl);
+}
